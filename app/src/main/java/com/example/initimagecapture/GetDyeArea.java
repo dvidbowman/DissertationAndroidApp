2cec19a0832a79fe6e917f7ab5f302b124f7073f
40257635 <dbowman03@qub.ac.uk>
2022-01-31 16:15:29 +0000

    Ability to crop image further using User Touch

--- app/src/main/java/com/example/initimagecapture/GetDyeArea.java
+++ app/src/main/java/com/example/initimagecapture/GetDyeArea.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,9 +15,9 @@ import android.widget.TextView;
 
 public class GetDyeArea extends AppCompatActivity {
     // Controls
-    private TextView disp_textView;
+    private TextView screenCoord_textView, bitmapCoord_textView;
     private Button analyse_btn;
-    private ImageView crop_imgv;
+    private ImageView crop_imgv, dyedArea_imgv;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -26,16 +27,76 @@ public class GetDyeArea extends AppCompatActivity {
 
         analyse_btn = (Button) findViewById(R.id.button_analyseImage);
         crop_imgv = (ImageView) findViewById(R.id.imageView_crop);
-        disp_textView = (TextView) findViewById(R.id.textView_disp);
+        dyedArea_imgv = (ImageView) findViewById(R.id.imageView_dyedArea);
+        screenCoord_textView = (TextView) findViewById(R.id.textView_screenCoord);
+        bitmapCoord_textView = (TextView) findViewById(R.id.textView_bitmapCoord);
 
-        Bitmap bitmap = BitmapFactory.decodeByteArray(User.getCroppedImageByteArray(), 0, User.getCroppedImageByteArray().length);
-        crop_imgv.setImageBitmap(bitmap);
+        Bitmap srcBitmap = BitmapFactory.decodeByteArray(User.getCroppedImageByteArray(), 0, User.getCroppedImageByteArray().length);
+        crop_imgv.setImageBitmap(srcBitmap);
 
         crop_imgv.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                disp_textView.setText("Touch coordinates : " +
-                        String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
+                float eventX = event.getX();
+                float eventY = event.getY();
+                float[] eventXY = new float[] {eventX, eventY};
+
+                Matrix invertMatrix = new Matrix();
+                ((ImageView) v).getImageMatrix().invert(invertMatrix);
+
+                invertMatrix.mapPoints(eventXY);
+                int bitmapX = Integer.valueOf((int) eventXY[0]);
+                int bitmapY = Integer.valueOf((int) eventXY[1]);
+
+                if (bitmapX < 0) {
+                    bitmapX = 0;
+                } else if (bitmapX > srcBitmap.getWidth() - 1) {
+                    bitmapX = srcBitmap.getWidth() - 1;
+                }
+
+                if (bitmapY < 0) {
+                    bitmapY = 0;
+                } else if (bitmapY > srcBitmap.getHeight() - 1) {
+                    bitmapY = srcBitmap.getHeight() - 1;
+                }
+
+                int dyedAreaTopLeftX;
+                int dyedAreaTopLeftY;
+                int dyedAreaTopRightX;
+                int dyedAreaBottomRightY;
+
+                if (bitmapX - 80 < 0) {
+                    dyedAreaTopLeftX = 0;
+                } else {
+                    dyedAreaTopLeftX = bitmapX - 80;
+                }
+
+                if (bitmapY - 50 < 0) {
+                    dyedAreaTopLeftY = 0;
+                } else {
+                    dyedAreaTopLeftY = bitmapY - 50;
+                }
+
+                if (bitmapX + 80 > srcBitmap.getWidth()) {
+                    dyedAreaTopRightX = srcBitmap.getWidth();
+                } else {
+                    dyedAreaTopRightX = bitmapX + 80;
+                }
+
+                if (bitmapY + 80 > srcBitmap.getHeight()) {
+                    dyedAreaBottomRightY = srcBitmap.getHeight();
+                } else {
+                    dyedAreaBottomRightY = bitmapY + 80;
+                }
+
+                screenCoord_textView.setText("Screen touch coordinates : " +
+                        String.valueOf(eventXY[0]) + ", " + String.valueOf(eventXY[1]));
+
+                bitmapCoord_textView.setText("Bitmap touch coordinates : " +
+                        String.valueOf(bitmapX) + ", " + String.valueOf(bitmapY));
+
+                Bitmap areaToAnalyse = Bitmap.createBitmap(srcBitmap, dyedAreaTopLeftX, dyedAreaTopLeftY, (dyedAreaTopRightX - dyedAreaTopLeftX), (dyedAreaBottomRightY - dyedAreaTopLeftY));
+                dyedArea_imgv.setImageBitmap(areaToAnalyse);
 
                 return true;
             }
